ce224484585bda6c5edbaca443377281533210fc
Kailang Fu <kailangfu@gmail.com>
2018-10-10 00:47:36 -0500

    Move GpController to Futilef

---
+++ Assets/Example.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Futilef;
+
+using ImgAttr = Futilef.GpController.ImgAttr;
+
+public class Example : MonoBehaviour {
+	GpController gpc;
+
+	void OnEnable() {
+		gpc = new GpController();
+		gpc.Init();
+
+		gpc.AddImg(1, 10001);
+		gpc.SetImgAttr(1, ImgAttr.Alpha, 1f);
+		gpc.SetImgAttrEased(1, ImgAttr.Scale, 5f, EsType.BackInOut, 0.01f, 0.01f);
+		gpc.Wait(.5f);
+		gpc.SetImgAttr(1, ImgAttr.Alpha, 0f);
+		gpc.Wait(.5f);
+		gpc.SetImgAttr(1, ImgAttr.Alpha, 1f);
+		gpc.Wait(1f);
+		gpc.SetImgAttrEased(1, ImgAttr.Alpha, 2f, EsType.BounceOut, 0f);
+		gpc.Wait();
+		gpc.SetImgAttrEased(1, ImgAttr.Alpha, 3f, EsType.BounceOut, 1f);
+		gpc.SetImgAttrEased(1, ImgAttr.Tint, 3f, EsType.BounceOut, 1f, 0.5f, 1f);
+		gpc.SetImgAttrEased(1, ImgAttr.Position, 4f, EsType.ElasticOut, 2f, -1f, 0f);
+		gpc.Wait();
+		gpc.SetImgAttrEased(1, ImgAttr.Tint, 3f, EsType.BounceOut, 1f, 1f, 1f);
+		gpc.SetImgAttrEased(1, ImgAttr.Position, 4f, EsType.ElasticOut, -2f, 2f, 0f);
+		gpc.SetImgAttrEased(1, ImgAttr.Rotation, 3f, EsType.ElasticOut, -10f);
+		gpc.Wait();
+		gpc.SetImgAttrEased(1, ImgAttr.Alpha, 4f, EsType.CubicOut, 0f);
+		gpc.Wait();
+		gpc.RmImg(1);
+	}
+
+	void Update() {
+		gpc.Update(Time.deltaTime);
+	}
+	 
+	void OnDisable() {
+		gpc.Dispose();
+		Debug.Log("Clean up ");
+	}
+}
