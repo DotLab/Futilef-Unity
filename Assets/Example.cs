1dd900c3c26c9c6f11e72c61a9fd28e7afa4114b
Kailang Fu <kailangfu@gmail.com>
2018-10-11 02:25:40 -0500

    Show double-brightness

--- Assets/Example.cs
+++ Assets/Example.cs
@@ -30,6 +30,8 @@ public class Example : MonoBehaviour {
 		gpc.SetImgAttrEased(1, ImgAttr.Position, 4f, EsType.ElasticOut, -2f, 2f, 0f);
 		gpc.SetImgAttrEased(1, ImgAttr.Rotation, 3f, EsType.ElasticOut, -10f);
 		gpc.Wait();
+		gpc.SetImgAttrEased(1, ImgAttr.Tint, 2f, EsType.ElasticOut, 1.5f, 1.5f, 1.5f);
+		gpc.Wait();
 		gpc.SetImgAttrEased(1, ImgAttr.Alpha, 4f, EsType.CubicOut, 0f);
 		gpc.Wait();
 		gpc.RmImg(1);
@@ -39,7 +41,7 @@ public class Example : MonoBehaviour {
 	}
 
 	void Update() {
-		Debug.LogFormat("{0} at {1} ({2})", "mouse", Input.mousePosition, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)));
+//		Debug.LogFormat("{0} at {1} ({2})", "mouse", Input.mousePosition, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)));
 
 		foreach (var t in Input.touches) {
 			Debug.LogFormat("{0} at {1} ({2})", t.fingerId, t.position, t.rawPosition);
