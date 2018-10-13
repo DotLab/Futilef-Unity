769b4e132bf0174486d074b75542816781ef3203
Kailang Fu <kailangfu@gmail.com>
2018-10-12 21:29:40 -0500

    Add tests

--- Assets/Example.cs
+++ Assets/Example.cs
@@ -7,51 +7,53 @@ public class Example : MonoBehaviour {
 	GpController gpc;
 
 	void OnEnable() {
+		#if DEBUG
+		System.Reflection.Assembly.GetAssembly(typeof(UnityEditor.SceneView)).GetType("UnityEditor.LogEntries").GetMethod("Clear").Invoke(new object(), null);
+
+		Algo.Test();
+		Lst.Test();
+		PtrLst.Test();
+		#endif
+
 		Res.LoadAtlases(10);
 
 		gpc = new GpController();
-		gpc.Init();
 
 		gpc.AddImg(1, 10001);
 		gpc.SetImgAttr(1, ImgAttr.Alpha, 1f);
-		gpc.SetImgAttrEased(1, ImgAttr.Scale, 5f, EsType.BackInOut, 0.01f, 0.01f);
+		gpc.SetImgAttrEased(1, ImgAttr.Scale, 1f, EsType.BackInOut, 0.01f, 0.01f);
 		gpc.Wait(.5f);
-		gpc.SetImgAttr(1, ImgAttr.Alpha, 0f);
-		gpc.Wait(.5f);
-		gpc.SetImgAttr(1, ImgAttr.Alpha, 1f);
-		gpc.Wait(1f);
-		gpc.SetImgAttrEased(1, ImgAttr.Alpha, 2f, EsType.BounceOut, 0f);
+		gpc.SetImgAttrEased(1, ImgAttr.Alpha, 1f, EsType.BounceOut, 0f);
 		gpc.Wait();
-		gpc.SetImgAttrEased(1, ImgAttr.Alpha, 3f, EsType.BounceOut, 1f);
-		gpc.SetImgAttrEased(1, ImgAttr.Tint, 3f, EsType.BounceOut, 1f, 0.5f, 1f);
-		gpc.SetImgAttrEased(1, ImgAttr.Position, 4f, EsType.ElasticOut, 2f, -1f, 0f);
+		gpc.SetImgAttrEased(1, ImgAttr.Alpha, 1.5f, EsType.BounceOut, 1f);
+		gpc.SetImgAttrEased(1, ImgAttr.Tint, 1.5f, EsType.BounceOut, 1f, 0.5f, 1f);
+		gpc.SetImgAttrEased(1, ImgAttr.Position, 2f, EsType.ElasticOut, 2f, -1f, 0f);
 		gpc.Wait();
-		gpc.SetImgAttrEased(1, ImgAttr.Tint, 3f, EsType.BounceOut, 1f, 1f, 1f);
-		gpc.SetImgAttrEased(1, ImgAttr.Position, 4f, EsType.ElasticOut, -2f, 2f, 0f);
-		gpc.SetImgAttrEased(1, ImgAttr.Rotation, 3f, EsType.ElasticOut, -10f);
+		gpc.SetImgAttrEased(1, ImgAttr.Tint, 1.5f, EsType.BounceOut, 1f, 1f, 1f);
+		gpc.SetImgAttrEased(1, ImgAttr.Position, 2f, EsType.ElasticOut, -2f, 2f, 0f);
+		gpc.SetImgAttrEased(1, ImgAttr.Rotation, 1.5f, EsType.ElasticOut, -10f);
+
+		gpc.AddImg(2, 10001);
+		gpc.SetImgAttr(2, ImgAttr.Position, 0f, 0f, 5f);
+		gpc.SetImgAttr(2, ImgAttr.Alpha, 1f);
+		gpc.SetImgAttrEased(2, ImgAttr.Scale, 1f, EsType.ElasticOut, 0.006f, 0.006f);
+		gpc.SetImgAttrEased(2, ImgAttr.Position, 4f, EsType.ElasticOut, -2f, 2f, 5f);
+
 		gpc.Wait();
-		gpc.SetImgAttrEased(1, ImgAttr.Tint, 2f, EsType.ElasticOut, 1.5f, 1.5f, 1.5f);
-		gpc.Wait();
-		gpc.SetImgAttrEased(1, ImgAttr.Alpha, 4f, EsType.CubicOut, 0f);
+		gpc.SetImgAttrEased(1, ImgAttr.Tint, 1f, EsType.ElasticOut, 1.5f, 1.5f, 1.5f);
+
 		gpc.Wait();
 		gpc.RmImg(1);
 
-		Input.simulateMouseWithTouches = true;
-		Debug.Log(Screen.currentResolution);
+		Application.targetFrameRate = 20;
 	}
 
 	void Update() {
-//		Debug.LogFormat("{0} at {1} ({2})", "mouse", Input.mousePosition, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)));
-
-		foreach (var t in Input.touches) {
-			Debug.LogFormat("{0} at {1} ({2})", t.fingerId, t.position, t.rawPosition);
-		}
-
 		gpc.Update(Time.deltaTime);
 	}
 	 
 	void OnDisable() {
-		gpc.Dispose();
 		Debug.Log("Clean up ");
+		gpc.Dispose();
 	}
 }
