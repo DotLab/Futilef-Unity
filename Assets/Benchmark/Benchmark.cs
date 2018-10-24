354852d341fa2609461c0c0670ca46130ca84534
Kailang <kailangfu@gmail.com>
2018-10-23 19:35:02 -0500

    Upgrade to 13f1 & Add benchmark for Lst

---
+++ Assets/Benchmark/Benchmark.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public abstract class Benchmark : MonoBehaviour {
+	System.Text.StringBuilder sb;
+	System.Diagnostics.Stopwatch sw;
+
+	void OnEnable() {
+		sb = new System.Text.StringBuilder();
+		sb.AppendFormat("{0} test start\n", GetTestName());
+		var sw2 = new System.Diagnostics.Stopwatch(); sw2.Stop(); sw2.Reset();
+		sw = new System.Diagnostics.Stopwatch(); sw.Stop(); sw.Reset(); sw.Start();
+		RunTests();
+		sb.AppendFormat("{0} test end, {1} ms\n", GetTestName(), sw2.ElapsedMilliseconds);
+		Debug.Log(sb.ToString());
+	}
+
+	protected abstract string GetTestName();
+	protected abstract void RunTests();
+
+	protected void StartCase() {
+		sw.Stop();
+		sw.Reset();
+		sw.Start();
+	}
+
+	long refTime;
+	protected void RefCase() {
+		refTime = sw.ElapsedTicks;
+	}
+	protected void LogCase(string name) {
+		long time = sw.ElapsedTicks;
+		sb.AppendFormat("\t{0} - {1}: {2:N0} ref\n\t{0} - {1}: {3:N0} {4}\n", GetTestName(), name, refTime, time, time <= refTime ? ":-)" : ":-(");
+	}
+}
--- Assets/Example.cs
+++ Assets/Example.cs
@@ -45,92 +45,10 @@ public unsafe class Example : MonoBehaviour {
 			gpc.Wait(.5f);
 		}
 
-//		Application.targetFrameRate = 60;
-		lastCounterTime = Time.time;
-		frameCounter = 0;
-
-		var sw = new System.Diagnostics.Stopwatch();
-		var refDict = new System.Collections.Generic.Dictionary<uint, uint>();
-		sw.Stop();
-		sw.Reset();
-		sw.Start();
-		for (uint i = 2; i < 1000000; i += 1) {
-			refDict.Add(i, i);
-		}
-		sw.Stop();
-		UnityEngine.Debug.LogFormat("refDict Test: {0:N0}", sw.ElapsedTicks);
-
-		var dict = stackalloc Dict[1]; Dict.Init(dict);
-		sw.Reset();
-		sw.Start();
-		for (uint i = 2; i < 1000000; i += 1) {
-			Dict.Set(dict, i, (void *)i, (void *)i, eq);
-		}
-		sw.Stop();
-		UnityEngine.Debug.LogFormat("dict Test: {0:N0}", sw.ElapsedTicks);
-
-		sw.Reset();
-		sw.Start();
-		for (uint i = 2; i < 1000000; i += 1) {
-			refDict.Remove(i);
-		}
-		sw.Stop();
-		UnityEngine.Debug.LogFormat("refDict Test: {0:N0}", sw.ElapsedTicks);
-
-		sw.Reset();
-		sw.Start();
-		for (uint i = 2; i < 1000000; i += 1) {
-			Dict.Remove(dict, i, (void *)i, eq);
-		}
-		sw.Stop();
-		UnityEngine.Debug.LogFormat("dict Test: {0:N0}", sw.ElapsedTicks);
-
-		sw.Reset();
-		sw.Start();
-		for (uint i = 2; i < 1000000; i += 1) {
-			refDict.Add(i, i);
-		}
-		sw.Stop();
-		UnityEngine.Debug.LogFormat("refDict Test: {0:N0}", sw.ElapsedTicks);
-
-		sw.Reset();
-		sw.Start();
-		for (uint i = 2; i < 1000000; i += 1) {
-			Dict.Set(dict, i, (void *)i, (void *)i, eq);
-		}
-		sw.Stop();
-		UnityEngine.Debug.LogFormat("dict Test: {0:N0}", sw.ElapsedTicks);
-
-		uint k = 0;
-		sw.Reset();
-		sw.Start();
-		for (uint i = 2; i < 1000000; i += 1) {
-			k += refDict[i];
-		}
-		sw.Stop();
-		UnityEngine.Debug.LogFormat("refDict Test: {0:N0}", sw.ElapsedTicks);
-
-		k = 0;
-		sw.Reset();
-		sw.Start();
-		for (uint i = 2; i < 1000000; i += 1) {
-			k += (uint)Dict.Get(dict, i, (void *)i, eq);
-		}
-		sw.Stop();
-		UnityEngine.Debug.LogFormat("dict Test: {0:N0}", sw.ElapsedTicks);
-	}
-	static unsafe bool eq(void *a, void *b) {
-		return (uint)a == (uint)b;
+		Application.targetFrameRate = 60;
 	}
 
 	void Update() {
-		frameCounter += 1;
-		if (frameCounter >= frameCounterSize) {
-//			Fdb.Log("{0:F2} fps", frameCounterSize / (Time.time - lastCounterTime));
-			lastCounterTime = Time.time;
-			frameCounter = 0;
-		}
-
 		gpc.Update(Time.deltaTime);
 	}
 	 
